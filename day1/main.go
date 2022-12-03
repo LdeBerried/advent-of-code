a13acd669ae917c767ca690641debb9d8b6e77c4
Laura Alcober <laalpri@gmail.com>
2022-12-04 00:03:08 +0100

    feat: add day 1 solution

--- day1/main.go
+++ day1/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -10,7 +13,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(inputContent)
+
+	rawCaloriesArray := strings.Split(inputContent, "\n")
+	groupedCaloriesArray := make([]int, 0, 1)
+
+	elfCalories := 0
+
+	for _, s := range rawCaloriesArray {
+		if s != "" {
+			intValue, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			elfCalories += intValue
+		} else {
+			groupedCaloriesArray = append(groupedCaloriesArray, elfCalories)
+			elfCalories = 0
+		}
+	}
+
+	sort.Ints(groupedCaloriesArray)
+
+	fmt.Printf("The elf carrying most calories is carrying %d! \n", groupedCaloriesArray[len(groupedCaloriesArray)-1])
 }
 
 func ReadFile(fileName string) (string, error) {
