a1ab7dcf154a429cc8ad16f41271f9680f95c01a
Laura Alcober <laalpri@gmail.com>
2024-12-01 12:44:46 +0100

    refactor: renaming functions and adding codestring

--- 2024/day_1/main.py
+++ 2024/day_1/main.py
@@ -1,4 +1,8 @@
 def get_locations() -> tuple[list[int], list[int]]:
+    """
+    Retrieve int lists from plain text file
+    :return: 2 int lists ordered from lowest to highest
+    """
     first_list = []
     second_list = []
     with open("location_list", "r") as location_file:
@@ -12,7 +16,14 @@ def get_locations() -> tuple[list[int], list[int]]:
     return first_list, second_list
 
 
-def first_challenge(first_location_list: list[int], second_location_list: list[int]) -> int:
+def get_location_distances_from_lists(first_location_list: list[int], second_location_list: list[int]) -> int:
+    """
+    Given 2 int lists with locations, return the sum of distance between same ordered items in both lists so the result is always a positive number since it is a distance.
+    i.e.: (first_element_list_a = 3, first_element_list_b = 2, distance = 1), (second_element_list_a = 3, second_element_list_b = 6, distance = 3)
+    :param first_location_list: integer list with n elements
+    :param second_location_list: integer list with n elements
+    :return: sum of distances between elements in one list against the other
+    """
     location_distance = 0
     for first_location, second_location in zip(first_location_list, second_location_list):
         if first_location > second_location:
@@ -20,13 +31,13 @@ def first_challenge(first_location_list: list[int], second_location_list: list[i
         elif second_location > first_location:
             location_distance += second_location - first_location
 
-    return location_distance # 1223326
+    return location_distance
 
 
 def main():
     first_location_list, second_location_list = get_locations()
-    first_challenge_result = first_challenge(first_location_list,second_location_list)
-    print(first_challenge_result)
+    location_distances = get_location_distances_from_lists(first_location_list, second_location_list)
+    print(location_distances)
 
 
 if __name__ == "__main__":
