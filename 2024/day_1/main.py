1d37722ea160aaf1de7d867f2ee96a76c2eada65
Laura Alcober <laalpri@gmail.com>
2024-12-01 13:07:12 +0100

    refactor: simplify distance calculation and rename function

--- 2024/day_1/main.py
+++ 2024/day_1/main.py
@@ -16,7 +16,7 @@ def get_locations() -> tuple[list[int], list[int]]:
     return first_list, second_list
 
 
-def get_location_distances_from_lists(first_location_list: list[int], second_location_list: list[int]) -> int:
+def get_location_distance_from_lists(first_location_list: list[int], second_location_list: list[int]) -> int:
     """
     Given 2 int lists with locations, return the sum of distance between same ordered items in both lists so the result is always a positive number since it is a distance.
     i.e.: (first_element_list_a = 3, first_element_list_b = 2, distance = 1), (second_element_list_a = 3, second_element_list_b = 6, distance = 3)
@@ -26,17 +26,13 @@ def get_location_distances_from_lists(first_location_list: list[int], second_loc
     """
     location_distance = 0
     for first_location, second_location in zip(first_location_list, second_location_list):
-        if first_location > second_location:
-            location_distance += first_location - second_location
-        elif second_location > first_location:
-            location_distance += second_location - first_location
-
+        location_distance += abs(first_location-second_location)
     return location_distance
 
 
 def main():
     first_location_list, second_location_list = get_locations()
-    location_distances = get_location_distances_from_lists(first_location_list, second_location_list)
+    location_distances = get_location_distance_from_lists(first_location_list, second_location_list)
     print(location_distances)
 
 
